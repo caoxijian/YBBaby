6f7f7210bed97119ef505b22f854d9065959595e
caojian <1092398284@qq.com>
2018-04-27 17:37:32 +0800

    更改框架

---
+++ YBBaby/YBDataFoundation/YBDataFoundation/YBDataFoundation.h
@@ -0,0 +1,19 @@
+//
+//  YBDataFoundation.h
+//  YBDataFoundation
+//
+//  Created by caojian on 2018/4/27.
+//  Copyright © 2018年 caojian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YBDataFoundation.
+FOUNDATION_EXPORT double YBDataFoundationVersionNumber;
+
+//! Project version string for YBDataFoundation.
+FOUNDATION_EXPORT const unsigned char YBDataFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YBDataFoundation/PublicHeader.h>
+
+
